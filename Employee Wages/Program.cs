53dd671b370e4082c87a2f4d77816bd22fa4bc94
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-26 04:48:59 -0700

    [tejas] add calculated total wage

--- Employee Wages/Program.cs
+++ Employee Wages/Program.cs
@@ -9,20 +9,40 @@ namespace Employee_Wages
 
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
-            //variable
-            int empHrs ;
-            int empWage = 0;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-           
-            empWage = empHrs * EMP_RATE_PER_HOUR*20;
+            int DAYS = 1;
 
-            Console.WriteLine("Employee wage = " + empWage);
+            //variable
+            int totalHrs = 0;
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+          
+
+            //Computation
+           
+
+            for (DAYS = 1; DAYS <= 20; DAYS += 1)
+            {
+                while (totalHrs <= 100)
+                {
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 2);
+                    if (empCheck == IS_FULL_TIME)
+                    {
+                        empHrs = 8;
+                        totalHrs += empHrs;
+                    }
+                    else
+                    {
+                        empHrs = 4;
+                        totalHrs += empHrs;
+                    }
+                }
+                empWage = totalHrs * EMP_RATE_PER_HOUR * DAYS;
+                totalEmpWage += empWage;
+            }
+            Console.WriteLine("Total Employee Wage = " + totalEmpWage);
         }
     }
 }
